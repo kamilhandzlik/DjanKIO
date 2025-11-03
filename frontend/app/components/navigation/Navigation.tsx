836ea12f87fb0977d2107a93cf31a893135dc42b
kamol0209 <kamol0209@gmail.com>
2025-11-03 18:57:41 +0100

    changed backend urls.py | Navigation.tsx and page.tsx

--- frontend/app/components/navigation/Navigation.tsx
+++ frontend/app/components/navigation/Navigation.tsx
@@ -1,7 +1,10 @@
 import Link from "next/link";
 import Image from "next/image";
+import { useState } from "react";
 
 const Navigation = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <nav className="flex justify-between items-center space-x-2">
       <div className="bg-white rounded-full px-3 hover:bg-gray-200 transition-colors">
@@ -19,6 +22,82 @@ const Navigation = () => {
             d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
           />
         </svg>
+        <div className=" absolute top-[110px] left-2  bg-white border border-gray-300 shadow-md rounded-2xl cursor-pointer grid sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-5">
+          <div className="p-4 border ">
+            <h3 className="text-black text-bold text-2xl">BAZA WIEDZY</h3>
+            <h4 className="text-gray-500 hover:bg-gray-200">
+              <a href="">orzecznictwo</a>
+            </h4>
+            <h4 className="text-gray-500 hover:bg-gray-200">
+              <a href="">ustawa Pzp</a>
+            </h4>
+            <h4 className="text-gray-500 hover:bg-gray-200">
+              <a href="">zawartość</a>
+            </h4>
+          </div>
+          <div className="p-4 border ">
+            <h3 className="text-black text-bold text-2xl">AKTUALNOŚCI</h3>
+            <h4 className="text-gray-500 hover:bg-gray-200">
+              <a href="">blog</a>
+            </h4>
+            <h4 className="text-gray-500 hover:bg-gray-200">
+              <a href="">popularne</a>
+            </h4>
+            <h4 className="text-gray-500 hover:bg-gray-200">
+              <a href="">ciekawe</a>
+            </h4>
+            <h4 className="text-gray-500 hover:bg-gray-200">
+              <a href="">nowe</a>
+            </h4>
+            <h4 className="text-gray-500 hover:bg-gray-200">
+              <a href="">wokanda</a>
+            </h4>
+          </div>
+          <div className="p-4 border ">
+            <h3 className="text-black text-bold text-2xl">NARZĘDZIA</h3>
+            <h4 className="text-gray-500 hover:bg-gray-200">
+              <a href="">kody CPV</a>
+            </h4>
+            <h4 className="text-gray-500 hover:bg-gray-200">
+              <a href="">indeks tematyczny</a>
+            </h4>
+            <h4 className="text-gray-500 hover:bg-gray-200">
+              <a href="">sumy kontrolne</a>
+            </h4>
+            <h4 className="text-gray-500 hover:bg-gray-200">
+              <a href="">analiza PDF/PAdES</a>
+            </h4>
+            <h4 className="text-gray-500 hover:bg-gray-200">
+              <a href="">walidacja</a>
+            </h4>
+
+            <h4 className="text-gray-500 hover:bg-gray-200">
+              <a href="">terminy</a>
+            </h4>
+
+            <h4 className="text-gray-500 hover:bg-gray-200">
+              <a href="">wzory</a>
+            </h4>
+          </div>
+          <div className="p-4 border ">
+            <h3 className="text-black text-bold text-2xl">EDUKACJA</h3>
+            <h4 className="text-gray-500 hover:bg-gray-200">
+              <a href="">SpotKIO</a>
+            </h4>
+          </div>
+          <div className="p-4 border ">
+            <h3 className="text-black text-bold text-2xl">O DJANKIO</h3>
+            <h4 className="text-gray-500 hover:bg-gray-200">
+              <a href="">cennik</a>
+            </h4>
+            <h4 className="text-gray-500 hover:bg-gray-200">
+              <a href="">kontakt</a>
+            </h4>
+            <h4 className="text-gray-500 hover:bg-gray-200">
+              <a href="">zawartość</a>
+            </h4>
+          </div>
+        </div>
       </div>
 
       <Link href="/">
