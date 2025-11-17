d0beb29c17311d9e9d23fc5df067ef240125f8e5
kamol0209 <kamol0209@gmail.com>
2025-11-17 18:07:53 +0100

    changed svg in searchbar

--- frontend/app/components/search_field/search_bar.tsx
+++ frontend/app/components/search_field/search_bar.tsx
@@ -41,14 +41,14 @@ const SearchBar = () => {
               xmlns="http://www.w3.org/2000/svg"
               fill="none"
               viewBox="0 0 24 24"
-              strokeWidth={2}
-              stroke="white"
-              className="w-6 h-6"
+              strokeWidth="1.5"
+              stroke="currentColor"
+              className="size-6"
             >
               <path
                 strokeLinecap="round"
                 strokeLinejoin="round"
-                d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"
+                d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z"
               />
             </svg>
           </button>
