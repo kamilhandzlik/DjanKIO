c9acd3f920e5ded33585801901f92050de65369b
kamol0209 <kamol0209@gmail.com>
2025-11-24 19:09:44 +0100

    changed price tag page and added signature banner

--- frontend/app/pricetag/page.tsx
+++ frontend/app/pricetag/page.tsx
@@ -1,8 +1,26 @@
+"use client";
+import Image from "next/image";
+
+import SignatureBanner from "../components/PricetagContent/signatureBanner";
+
 const PriceTagPage = () => {
   return (
     <>
-      <div className="bg-white">
-        <h1 className="text-black">Price Tag Page</h1>
+      <div className="min-h-screen w-full bg-zinc-50 flex flex-col gap-0.5">
+        <div className="w-[200px] h-[200px] items-center justify-center mx-auto py-10">
+          <Image
+            src="/djankio-mark-flat.png"
+            alt="Price Tag Banner"
+            width={200}
+            height={200}
+            className=" object-cover"
+          />
+        </div>
+        <h1 className="text-center text-3xl text-gray-600 mt-4">
+          Zamów plan dopasowany do Twoich potrzeb
+        </h1>
+
+        <SignatureBanner />
       </div>
     </>
   );
