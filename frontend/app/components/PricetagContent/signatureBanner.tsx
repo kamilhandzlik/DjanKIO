558699782913b0bc2872b4a281a6d21d1866ab0b
kamol0209 <kamol0209@gmail.com>
2025-11-24 19:09:44 +0100

    changed price tag page and added signature banner

--- frontend/app/components/PricetagContent/signatureBanner.tsx
+++ frontend/app/components/PricetagContent/signatureBanner.tsx
@@ -9,6 +9,10 @@ const SignatureBanner = () => {
       href="/pricetag"
       className="group w-full max-w-[1200px] mx-auto border border-gray-300 rounded-md overflow-hidden cursor-pointer grid grid-cols-1 md:grid-cols-2 bg-white"
     >
+<<<<<<< HEAD
+=======
+      {/* LEWA STRONA */}
+>>>>>>> c9acd3f (changed price tag page and added signature banner)
       <div className="p-10 flex flex-col gap-4 justify-center">
         <h2 className="text-2xl md:text-3xl font-semibold text-gray-700 leading-snug">
           Chcesz potwierdzić ważność podpisu elektronicznego?
@@ -39,6 +43,10 @@ const SignatureBanner = () => {
         </div>
       </div>
 
+<<<<<<< HEAD
+=======
+      {/* PRAWA STRONA — OBRAZ */}
+>>>>>>> c9acd3f (changed price tag page and added signature banner)
       <div className="relative w-full h-[220px] md:h-full">
         <Image
           src="/writing-man.png"
