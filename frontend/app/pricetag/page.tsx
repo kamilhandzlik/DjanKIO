2a67dfec7bfa3314ec80dc0638f52aaf0ee3ebd6
kamol0209 <kamol0209@gmail.com>
2025-11-24 18:42:05 +0100

    changed layout.tsx and added pricetag page.tsx

---
+++ frontend/app/pricetag/page.tsx
@@ -0,0 +1,11 @@
+const PriceTagPage = () => {
+  return (
+    <>
+      <div className="bg-white">
+        <h1 className="text-black">Price Tag Page</h1>
+      </div>
+    </>
+  );
+};
+
+export default PriceTagPage;
