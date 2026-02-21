12c233316fdf7d42a120d99ad10be899815d68c6
kamol0209 <kamol0209@gmail.com>
2026-02-21 09:54:27 +0100

    conteneraized backend

--- frontend/app/ciekawe/page.tsx
+++ frontend/app/ciekawe/page.tsx
@@ -1,13 +1,6 @@
 "use client";
 import Link from "next/link";
-<<<<<<< HEAD
-<<<<<<< HEAD
 import Wokanda from "../components/wokanda/wokanda";
-=======
->>>>>>> ddceb75 (changed bg-color in global css and added ciekawe page)
-=======
-import Wokanda from "../components/wokanda/wokanda";
->>>>>>> 9bb9ac6 (added wokanda component created wokanda page and added wokanda to main, pricetag and new)
 
 const CiekawePage = () => {
   return (
@@ -73,14 +66,8 @@ const CiekawePage = () => {
           </h1>
         </Link>
       </div>
-<<<<<<< HEAD
-<<<<<<< HEAD
+
       <Wokanda />
-=======
->>>>>>> ddceb75 (changed bg-color in global css and added ciekawe page)
-=======
-      <Wokanda />
->>>>>>> 9bb9ac6 (added wokanda component created wokanda page and added wokanda to main, pricetag and new)
     </div>
   );
 };
